a55721c61facbac78abbb9072638b7ed5a02ffad
andrewnnov <andrewnnov@yandex.ru>
2023-09-10 14:16:13 -0300

    work with zip, csv, json

--- src/test/java/guru/qa/FilesParsingText.java
+++ src/test/java/guru/qa/FilesParsingText.java
@@ -3,15 +3,25 @@ package guru.qa;
 import com.codeborne.pdftest.PDF;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.xlstest.XLS;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.opencsv.CSVReader;
+import guru.qa.model.Glossary;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import java.io.*;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
 
 import static com.codeborne.selenide.Selenide.$;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
+
 public class FilesParsingText {
+
+    ClassLoader cl = FilesParsingText.class.getClassLoader();
 
     @Disabled
     @Test
@@ -29,5 +39,53 @@ public class FilesParsingText {
         XLS content = new XLS(xlsxFile);
         assertThat(content.excel.getSheetAt(0).getRow(1).getCell(3).getStringCellValue())
                 .contains("Something");
+    }
+
+    @Test
+    void csvParseTest() throws Exception {
+        try(InputStream resource = cl.getResourceAsStream("qa_guru.csv");
+            CSVReader reader = new CSVReader(new InputStreamReader(resource))) {
+            List<String[]> content = reader.readAll();
+            assertThat(content.get(0)[1]).contains("lessons");
+        }
+    }
+
+    @Test
+    void zipParseTest() throws Exception {
+        try(InputStream resource = cl.getResourceAsStream("goal.zip");
+            ZipInputStream zis = new ZipInputStream(resource);
+            ) {
+            ZipEntry entry;
+            while ((entry = zis.getNextEntry()) != null) {
+                assertThat(entry.getName()).isEqualTo("goal.png");
+            }
+        }
+    }
+
+    @Test
+    void jsonParseTest() throws Exception {
+        Gson gson = new Gson();
+        try(
+            InputStream resource = cl.getResourceAsStream("glossary.json");
+            InputStreamReader reader = new InputStreamReader(resource)) {
+               JsonObject jsonObject = gson.fromJson(reader, JsonObject.class);
+               assertThat(jsonObject.get("title").getAsString()).isEqualTo("example glossary");
+               assertThat(jsonObject.get("GlossDiv").getAsJsonObject().get("title").getAsString()).isEqualTo("S");
+               assertThat(jsonObject.get("GlossDiv").getAsJsonObject().get("flag").getAsBoolean()).isTrue();
+        }
+    }
+
+    @Test
+    void jsonParseImprovedTest() throws Exception {
+        Gson gson = new Gson();
+        try(
+                InputStream resource = cl.getResourceAsStream("glossary.json");
+                InputStreamReader reader = new InputStreamReader(resource)
+        ) {
+            Glossary jsonObject = gson.fromJson(reader, Glossary.class);
+            assertThat(jsonObject.title).isEqualTo("example glossary");
+            assertThat(jsonObject.glossDiv.title).isEqualTo("S");
+            assertThat(jsonObject.glossDiv.flag).isTrue();
+        }
     }
 }
